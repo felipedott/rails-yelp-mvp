c54b4d6cd6cb8bc65a79cc07267c3f155b7e7c22
Felipe Dottling <felipedott@gmail.com>
2022-11-16 10:30:30 -0500

    so falta refatorar

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :restaurants
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'restaurants#index'
+  resources :restaurants do
+    resources :reviews, only: %i[new create index]
+  end
+  resources :reviews, only: [:show]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Restaurant.create!(name: "Nonno's Italian Restaurant", address: '1140 E Altamonte Dr, St 1018', category: 'italian')
+Restaurant.create!(name: "Antica Pizzeria", address: '249 W State Hwy 436 Ste 1044', category: 'belgian')
+Restaurant.create!(name: "Tibbys New Orleans", address: '494 W State Rd 436', category: 'chinese')
+Restaurant.create!(name: "The Crepevine", address: '249 W State Rd 436 Ste 1117', category: 'french')
+Restaurant.create!(name: "Santiagos Bodega", address: '1185 Springs Centre S', category: 'japanese')
